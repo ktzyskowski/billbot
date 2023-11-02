4a264d73267f9299655a4cd6b6016dff52cb08c0
ktzyskowski <ktzyskowski@gmail.com>
2023-11-01 22:48:50 -0400

    Change bot to scan for messages from Bill instead of me

--- billbot2.py
+++ billbot2.py
@@ -1,7 +1,6 @@
 import discord
 import pytz
 import os
-from discord import Message, Member
 from discord.ext import commands
 from discord.ext.commands import Context
 from datetime import datetime
@@ -10,6 +9,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
+bot.activity = "Checking the last time $bill sent a new game"
 
 
 def now():
@@ -18,9 +18,9 @@ def now():
 
 @bot.command()
 async def bill(ctx: Context):
-    me = await ctx.guild.fetch_member(498870829095059476)
+    bill = await ctx.guild.fetch_member(99631940310663168)
     async for msg in ctx.channel.history(limit=10_000):
-        if msg.author == me and ("https://store.steampowered.com" in msg.content or "http://store.steampowered.com" in msg.content):
+        if msg.author == bill and ("https://store.steampowered.com" in msg.content or "http://store.steampowered.com" in msg.content):
             days = (now() - msg.created_at).days
             days_str = "1 day" if days == 1 else f"{days} days"
             await ctx.reply(f"The last time Bill sent a new Steam game recommendation in {msg.channel.mention} was {days_str} ago\n{msg.jump_url}")
