9ec420ddcba24ad4224c880466c444ba176ffb08
ktzyskowski <ktzyskowski@gmail.com>
2023-11-01 22:51:20 -0400

    Fix activity

--- billbot2.py
+++ billbot2.py
@@ -9,7 +9,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
-bot.activity = "Checking the last time $bill sent a new game"
+bot.activity = discord.Activity(
+    type=discord.ActivityType.listening, name="Checking the last time $bill sent a new game")
 
 
 def now():
