f4919bd1039b78bc7eb2badd191bbdd26aa0821a
ktzyskowski <ktzyskowski@gmail.com>
2023-11-02 13:49:31 -0400

    Add ethan command

--- billbot2.py
+++ billbot2.py
@@ -1,6 +1,9 @@
 import discord
 import pytz
 import os
+import json
+import requests as re
+from bs4 import BeautifulSoup as bs
 from discord.ext import commands
 from discord.ext.commands import Context
 from datetime import datetime
@@ -30,4 +33,32 @@ async def bill(ctx: Context):
             return
     await ctx.reply(f"I can't remember the last time Bill recommended a game :(")
 
-bot.run(os.environ['DISCORD_TOKEN'])
+
+@bot.hybrid_command()
+async def ethan(ctx: Context):
+    res = get_ethans_league_ranks()
+    if res is None:
+        await ctx.reply(f"I can't find Ethan's current League ranks :(")
+    else:
+        tier, division = res
+        await ctx.reply(f"Ethan's current solo/duo rank is {str.capitalize(tier)} {division}")
+
+
+def get_ethans_league_ranks():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+    web_page = re.get(
+        "https://www.op.gg/summoners/na/papa%20eef", headers=headers)
+    soup = bs(web_page.text, features="html.parser")
+    data = json.loads(soup.find(id="__NEXT_DATA__").text)[
+        "props"]["pageProps"]["data"]["league_stats"]
+    for queue in data:
+        if queue["queue_info"]["game_type"] == "SOLORANKED":
+            tier = queue["tier_info"]["tier"]
+            division = queue["tier_info"]["division"]
+            return tier, division
+    return None
+
+
+if __name__ == "__main__":
+    bot.run(os.environ['DISCORD_TOKEN'])
